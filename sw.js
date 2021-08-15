4eeba3d990a1306c4ee012ac284263ec21be75f3
Ben Reich <gerryslize@gmail.com>
2021-08-15 15:32:57 +1000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ef884e51100f3a6c0640.js"
+    "url": "webpack-runtime-aab8ba564762aa169e0e.js"
   },
   {
     "url": "framework-61f8197c4f77442a187e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ba07b7d879acc49dc38f5a0d7855cb87"
+    "revision": "da707de89d199ec5947b52033863dab0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-cd06cc959e74a365a713.js"
